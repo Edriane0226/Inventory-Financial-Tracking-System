3eeefdbc600e31409b20959bd7abae4eca2934ba
Edriane0226 <edriane.bangonon26@gmail.com>
2026-03-28 12:20:32 +0800

    Feat(model): create Receipt model for managing receipts data

---
+++ app/Models/Receipt.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class Receipt extends Model
+{
+    protected $table      = 'receipts';
+    protected $primaryKey = 'id';
+    protected $allowedFields = ['receipt_number', 'total_amount'];
+    protected $returnType = 'array';
+}
